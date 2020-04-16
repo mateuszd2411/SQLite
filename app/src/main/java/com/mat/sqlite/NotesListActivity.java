3e116868f4ad1f30554d93ceba34bd25ce29d9b5
Mateusz <mateuszd2411@gmail.com>
2020-04-16 16:54:54 +0200

    I added log

--- app/src/main/java/com/mat/sqlite/NotesListActivity.java
+++ app/src/main/java/com/mat/sqlite/NotesListActivity.java
@@ -3,12 +3,23 @@ package com.mat.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mat.sqlite.models.Note;
 
 public class NotesListActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
+
+        Note note = new Note("some title","some cement","timestamp");
+
+        Log.d(TAG,"onCreate: my note:" + note.toString());
+
+
     }
 }
