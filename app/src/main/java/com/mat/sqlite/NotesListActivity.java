04b12b4c19ccada27db36cbdacddc95389e2db6b
Mateusz <mateuszd2411@gmail.com>
2020-04-16 17:42:43 +0200

    I added arrayList recycler view to java activity

--- app/src/main/java/com/mat/sqlite/NotesListActivity.java
+++ app/src/main/java/com/mat/sqlite/NotesListActivity.java
@@ -1,25 +1,44 @@
 package com.mat.sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
+import com.mat.sqlite.adapters.NotesRecyclerAdapter;
 import com.mat.sqlite.models.Note;
+
+import java.util.ArrayList;
 
 public class NotesListActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
+
+    // Ui components
+    private RecyclerView mRecyclerView;
+
+    // vars
+    private ArrayList<Note> mNotes = new ArrayList<>();
+    private NotesRecyclerAdapter mNoteRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes_list);
 
-        Note note = new Note("some title","some cement","timestamp");
+        mRecyclerView = findViewById(R.id.recyclerView);
 
-        Log.d(TAG,"onCreate: my note:" + note.toString());
+        initRecyclerView();
 
 
+    }
+
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mNoteRecyclerAdapter = new NotesRecyclerAdapter(mNotes);
+        mRecyclerView.setAdapter(mNoteRecyclerAdapter);
     }
 }
