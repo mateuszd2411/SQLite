4f0a0ff5c492c5e966c2f44d246831d7a22dff0a
Mateusz <mateuszd2411@gmail.com>
2020-04-17 13:43:00 +0200

    get Intent with extra key

--- app/src/main/java/com/mat/sqlite/NoteActivity.java
+++ app/src/main/java/com/mat/sqlite/NoteActivity.java
@@ -3,12 +3,23 @@ package com.mat.sqlite;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mat.sqlite.models.Note;
 
 public class NoteActivity extends AppCompatActivity {
+
+    private static final String TAG = "selected";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        if (getIntent().hasExtra("selected_note")){
+            Note note = getIntent().getParcelableExtra("selected_note");
+            Log.d(TAG, "onCreate: selected");
+        }
+
     }
 }
