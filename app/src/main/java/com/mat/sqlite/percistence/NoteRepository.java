a241c7efec3474dbb07e4e99009a83831c180cc9
Mateusz <mateuszd2411@gmail.com>
2020-04-19 15:09:18 +0200

    I added NoteRepository.java and four method for that class

---
+++ app/src/main/java/com/mat/sqlite/percistence/NoteRepository.java
@@ -0,0 +1,36 @@
+package com.mat.sqlite.percistence;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+
+import com.mat.sqlite.models.Note;
+
+import java.util.List;
+
+public class NoteRepository {
+
+    private NoteDatabase mNoteDatabase;
+
+    public NoteRepository(Context context) {
+        mNoteDatabase = NoteDatabase.getInstance(context);
+    }
+
+    public void insertNoteTask(Note note){
+
+    }
+
+    public void updateNote(Note note){
+
+    }
+
+    public LiveData<List<Note>> retrieveNotesTask(){
+
+        return null;
+    }
+
+    public void deleteNote(){
+
+    }
+
+}
