a80ba40c82ed82e702c28fcb704b28179f92042c
Mateusz <mateuszd2411@gmail.com>
2020-04-22 14:33:28 +0200

    I added Contact model witch constructor, getter and setter, strings for parameters

---
+++ app/src/main/java/com/mat/sqlitetut/models/Contact.java
@@ -0,0 +1,69 @@
+package com.mat.sqlitetut.models;
+
+public class Contact {
+
+    private String name;
+    private String phoneNumber;
+    private String device;
+    private String email;
+    private String profileImage;
+
+    public Contact(String name, String phoneNumber, String device, String email, String profileImage) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.device = device;
+        this.email = email;
+        this.profileImage = profileImage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDevice() {
+        return device;
+    }
+
+    public void setDevice(String device) {
+        this.device = device;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", device='" + device + '\'' +
+                ", email='" + email + '\'' +
+                ", profileImage='" + profileImage + '\'' +
+                '}';
+    }
+}
