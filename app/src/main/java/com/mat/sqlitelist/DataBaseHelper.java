95a813df37f749d9c7b8f24e4b647a6ef9f494d6
Mateusz <mateuszd2411@gmail.com>
2020-04-24 10:44:51 +0200

    I added CREATE TABLE SQL and added static variable for this

--- app/src/main/java/com/mat/sqlitelist/DataBaseHelper.java
+++ app/src/main/java/com/mat/sqlitelist/DataBaseHelper.java
@@ -9,14 +9,22 @@ import androidx.annotation.Nullable;
 public class DataBaseHelper extends SQLiteOpenHelper {
 
 
+    public static final String CUSTOMER_TABLE = "CUSTOMER_TABLE";
+    public static final String COLUMN_CUSTOMER_NAME = "CUSTOMER_NAME";
+    public static final String COLUMN_CUSTOMER_AGE = "CUSTOMER_AGE";
+    public static final String COLUMN_ACTIVE_CUSTOMER = "ACTIVE_CUSTOMER";
+    public static final String COLUMN_ID = "ID";
+
     public DataBaseHelper(@Nullable Context context) {
         super(context, "customer.db", null, 1);
     }
 
     // this is called the first time a database is accessed. There should be code in here to create a new database.
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
+        String createTableStatement = "CREATE TABLE " + CUSTOMER_TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_CUSTOMER_NAME + " TEXT, " + COLUMN_CUSTOMER_AGE + " INT, " + COLUMN_ACTIVE_CUSTOMER + " BOOL)";
 
+        db.execSQL(createTableStatement);
     }
 
 
