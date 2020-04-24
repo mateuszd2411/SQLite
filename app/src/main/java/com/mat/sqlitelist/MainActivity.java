707d9eed5ef6c3c633d0c98a1e4fd8ffd0d9c2a6
Mateusz <mateuszd2411@gmail.com>
2020-04-24 11:14:07 +0200

    I added Exception error for try. Now I have files in my db.

--- app/src/main/java/com/mat/sqlitelist/MainActivity.java
+++ app/src/main/java/com/mat/sqlitelist/MainActivity.java
@@ -36,16 +36,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                CustomerModel customerModel;
+
                 try {
-                    CustomerModel customerModel = new CustomerModel(-1,et_name.getText().toString(),Integer.parseInt(et_age.getText().toString()),sw_activiteCustomer.isChecked());
+                    customerModel = new CustomerModel(-1,et_name.getText().toString(),Integer.parseInt(et_age.getText().toString()),sw_activiteCustomer.isChecked());
                     Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
 
                 }catch (Exception e){
                     Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                    customerModel = new CustomerModel(-1,"error",0,false);
+
+
+
                 }
 
                 DataBaseHelper dataBaseHelper = new DataBaseHelper(MainActivity.this);
 
+                boolean success = dataBaseHelper.addOne(customerModel);
+
+                Toast.makeText(MainActivity.this, "Success = " +success, Toast.LENGTH_SHORT).show();
 
             }
         });
