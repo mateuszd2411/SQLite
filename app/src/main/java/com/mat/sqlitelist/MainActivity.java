e764dd725096f364b81e40a391abd0f7f28e9545
Mateusz <mateuszd2411@gmail.com>
2020-04-24 09:53:51 +0200

    I added references to buttons and other controls on the layout. I set buttons and added secure to addButton

--- app/src/main/java/com/mat/sqlitelist/MainActivity.java
+++ app/src/main/java/com/mat/sqlitelist/MainActivity.java
@@ -3,12 +3,59 @@ package com.mat.sqlitelist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    /// references to buttons and other controls on the layout
+    Button btn_add, btn_viewAll;
+    EditText et_name, et_age;
+    Switch sw_activiteCustomer;
+    ListView lv_customersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_name = findViewById(R.id.et_name);
+        et_age = findViewById(R.id.et_age);
+        sw_activiteCustomer = findViewById(R.id.sw_active);
+        lv_customersList = findViewById(R.id.lv_customerList);
+
+        // button listeners for the add and view all buttons
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    CustomerModel customerModel = new CustomerModel(-1,et_name.getText().toString(),Integer.parseInt(et_age.getText().toString()),sw_activiteCustomer.isChecked());
+                    Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+
+                }catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Error creating customer", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
     }
 }
