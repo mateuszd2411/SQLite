df52cbca7b3c07268196979f415e53d2a44eed8a
Mateusz <mateuszd2411@gmail.com>
2020-04-29 16:12:03 +0200

    I added check permissions

--- app/src/main/java/com/mat/sqlitetut/MainActivity.java
+++ app/src/main/java/com/mat/sqlitetut/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mat.sqlitetut;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -15,6 +18,8 @@ import java.util.Map;
 public class MainActivity extends AppCompatActivity implements ViewContactsFragment.OnContactSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     public void OnContactSelected(Contact contact) {
@@ -60,5 +65,49 @@ public class MainActivity extends AppCompatActivity implements ViewContactsFragm
     private void InitImageLoader(){
         UniversalImageLoader universalImageLoader = new UniversalImageLoader(MainActivity.this);
         ImageLoader.getInstance().init(universalImageLoader.getConfig());
+    }
+
+    // Generalized method for asking permission. Can pass any array of permissions
+    public void veryfyPermisions(String[] permisions){
+        Log.d(TAG, "veryfyPermisions: asking user for permissions");
+        ActivityCompat.requestPermissions(
+                MainActivity.this,
+                permisions,
+                REQUEST_CODE
+        );
+    }
+
+    // Check to see if permission was granted for the passed parameters
+    // ONLY ONE PERMISSION MAY TO BE CHECKED AD A TIME
+    public boolean checkPermission(String[] permission){
+        Log.d(TAG, "checkPermission: checking permissions for: " + permission[0]);
+
+        int permissionRequest = ActivityCompat.checkSelfPermission(
+                MainActivity.this,
+                permission[0]);
+
+        if (permissionRequest != PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "checkPermission: \n Permission was not granted for " + permission[0]);
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: requestCode: " + requestCode);
+
+        switch (requestCode){
+            case REQUEST_CODE:
+                for (int i = 0; i < permissions.length; i++){
+                    if (grantResults[i] == PackageManager.PERMISSION_GRANTED){
+                        Log.d(TAG, "onRequestPermissionsResult: User has allowed permission to access: " + permissions[i]);
+                    }else {
+                        break;
+                    }
+                }
+                break;
+        }
     }
 }
