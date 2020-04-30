3bc07db368f4825f9e3b10ca04f3c3e79358961d
Mateusz <mateuszd2411@gmail.com>
2020-04-30 17:43:06 +0200

    I added DatabaseHelper with SQL

---
+++ app/src/main/java/com/mat/sqlitetut/Utils/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.mat.sqlitetut.Utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String TAG = "DatabaseHelper";
+
+    private static final String DATABASE_NAME = "contacts.db";
+    private static final String TABLE_NAME = "contacts_table";
+    public static final String COL0 = "ID";
+    public static final String COL1 = "NAME";
+    public static final String COL2 = "PHONE_NUMBER";
+    public static final String COL3 = "DEVICE";
+    public static final String COL4 = "EMAIL";
+    public static final String COL5 = "PROFILE_PHOTO";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE " +
+                TABLE_NAME + " ( " +
+                COL0 + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COL1 + " TEXT, " +
+                COL2 + " TEXT, " +
+                COL3 + " TEXT, " +
+                COL4 + " TEXT, " +
+                COL5 + " TEXT )";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP IF TABLE EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
